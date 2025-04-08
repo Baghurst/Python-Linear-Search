9154d9bc6b64e64c24c81c7a2c394f9ee1879c53
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-04-09 00:50:47 +0100

    Update PythonLinearSearch2.py

--- PythonLinearSearch2.py
+++ PythonLinearSearch2.py
@@ -1,7 +1,15 @@
-low = 0
-high = 15
-L = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
+L = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
 target_value = 28
-mid = low + high / 2
 
+low = 0
+high = len(L) - 1
 
+while low <= high:
+    mid = (low + high) // 2
+    if L[mid] == target_value:
+        print("Found at index:", mid)
+        break
+    elif L[mid] < target_value:
+        low = mid + 1
+    else:
+        high = mid - 1
